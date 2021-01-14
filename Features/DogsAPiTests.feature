ecbcf27b165dd7fddc5a66ed667c27661eba5ba0
James.Walters <waltersjames@icloud.com>
2021-01-14 20:35:54 +0000

    Simple tests for List All End Point in Dogs API

---
+++ Features/DogsAPiTests.feature
@@ -0,0 +1,11 @@
+﻿Feature: Dogs API Tests
+
+@basic
+Scenario: Status code is OK for valid request to LIST All
+	When the user submits a valid request to dog API to list all breeds
+	Then the status code is OK
+
+@content
+Scenario: Content type is JSON for valid request to LIST All
+	When the user submits a valid request to dog API to list all breeds
+	Then the content type is JSON
---
+++ Steps/DogTestsStepDefinitions.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using RestSharp;
+using System.Net;
+using TechTalk.SpecFlow;
+
+namespace RestSharpTest.Steps
+{
+    [Binding]
+    public class DogTestsStepDefinitions
+    {
+        IRestResponse response;
+
+        [When(@"the user submits a valid request to dog API to list all breeds")]
+        public void WhenTheUserSubmitsAValidRequestToDogAPIToListAllBreeds()
+        {
+            RestClient client  = new RestClient("https://dog.ceo");
+            RestRequest request = new RestRequest("api/breeds/list/all", Method.GET);
+
+            response = client.Execute(request);
+        }
+        
+        [Then(@"the status code is OK")]
+        public void ThenTheStatusCodeIsOK()
+        {
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+        
+        [Then(@"the content type is JSON")]
+        public void ThenTheContentTypeIsJSON()
+        {
+            response.ContentType.Should().Be("application/json");
+        }
+    }
+}
